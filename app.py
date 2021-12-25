0913c115d57bf287b34cf13b384cd243bdf7acf5
OffedulDev <aloneforever.commercial@gmail.com>
2021-12-25 18:55:12 +0100

    mhm some things

--- app.py
+++ app.py
@@ -1,18 +1,72 @@
-from flask import Flask, render_template, redirect, url_for
-
+from flask import Flask, render_template, redirect, url_for, session, request
 
 class LandingPage:
-    def __init__(self, _title, _color, _slogan):
+    def __init__(self, _title, _color, _slogan, _isLoggedIn):
         self.title = _title
         self.color = _color
         self.slogan = _slogan
+        self.isLoggedIn = _isLoggedIn
     
     def build(self):
-        return render_template('landingpage.html', title=self.title, color=self.color, slogan=self.slogan)
+        if self.isLoggedIn == True:
+            self.isLoggedIn = "Gestisci Account"
+        else:
+            self.isLoggedIn = "Registrati"
+        
+        return render_template('landingpage.html', title=self.title, color=self.color, slogan=self.slogan, stato=self.isLoggedIn)
+
+class AccountPage:
+    def __init__(self, _name, _password):
+        self._name = _name
+        self._password = str(_password.decode("utf-32"))
+    
+    def build(self):
+        return render_template('accountpage.html', self._name, self._password)
+
+class PreloadPage:
+    def __init__(self, redirect):
+        self.redirect = redirect
+    
+    def build(self):
+        return render_template('preload.html', redirect=self.redirect)
 
 app = Flask(__name__)
-
+app.config['secret_key'] = 'hghfhghirubruitbyutiyuryuid'
 
 @app.route("/")
 def init():
-    return LandingPage('Kich #foodforeverybody', 'DarkKhaki', 'Kich, food for everybody!').build()
+    try:
+        if session['isLoggedIn'] == True:
+            return LandingPage('Kich', 'DarkKhaki', 'Kich', True).build()
+    except:
+        return LandingPage('Kich', 'DarkKhaki', 'Kich', False).build()
+
+@app.route("/account")
+def account():
+    isLoggedIn = None
+
+    try:
+        isLoggedIn = session['isLoggedIn']
+
+        if isLoggedIn == True:
+            return redirect("/")
+        else:
+            isLoggedIn = True
+    except:
+        isLoggedIn = True
+    
+    if isLoggedIn != True: return redirect("/")
+
+    return redirect("/preload?redirect=registration&isLoggedIn=False")
+
+@app.route("/preload")
+def preload():
+    redirectPage = request.args.get('redirect')
+    isLoggedIn = request.args.get('isLoggedIn')
+    
+    return PreloadPage(redirectPage).build()
+
+@app.route("/registration")
+def registration():
+    return "d"
+    
