24eac2e03134dee7e1276389217cc9cd19c7d5ad
OffedulDev <aloneforever.commercial@gmail.com>
2021-12-25 13:39:10 +0100

    fixed app

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, redirect
 
+class HtmlObject:
+    def __init__(self, _object):
+        self.object = _object
+
+    def getObject(self):
+        return self.object
+
+class LandingPage:
+    def __init__(self, _title, _color, _slogan):
+        self.title = _title
+        self.color = _color
+        self.slogan = _slogan
+    
+    def build(self):
+        obj = HtmlObject('<html><head><title>' + self.title + '</title></head><body style="background-color: ' + self.color + '"><h1 style="text-align: center; font-family: Arial, Helvetica, sans-serif">' + self.title + '</h1></body></html>')
+
+        return obj.getObject()  
+1
 app = Flask(__name__)
+
 
 @app.route("/")
 def init():
-    return "test"
+    return LandingPage('LandTst', 'DarkKhaki', 'test').build()
